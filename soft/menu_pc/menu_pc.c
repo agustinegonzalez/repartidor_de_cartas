e20ec87f74ddc039bc2490dc39376b5dd5e4d12a
Agustin Emanuel Gonzalez <gonzalezagustin27@gmail.com>
2023-11-07 18:43:12 -0300

    Funcion UNO

--- soft/menu_pc/menu_pc.c
+++ soft/menu_pc/menu_pc.c
@@ -4,15 +4,26 @@
 #include <stdlib.h>
 #include "termset.h"
 #include <stdint.h>
+char* uno()
+{
+	char *cadena;
+	uint8_t cargapositiva = 0;
+       	scanf("%hhu", &cargapositiva);
+        if (cargapositiva == 1) {
+		
+	
+        }else
+		exit(1);
 
+	return (cadena = "UNO");
+}
 
 int main(int argc, char *argv[])
 {
 	int file_descriptor;
 	struct termios oldtty, newtty;
 	uint8_t opcion = 0;
-	int cargapositiva;
-
+	char *juego;
 
 	file_descriptor = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
 
@@ -41,19 +52,38 @@ int main(int argc, char *argv[])
 		if (opcion == 4) {
 			break;  // Salir del bucle cuando se ingresa "4"
 		}
-
+/*
 		if (opcion == 1) {
        			printf("¿Desea cargar el juego UNO?\n");
         		printf("1. Sí\n2. No\n");
        		 	scanf("%d", &cargapositiva);
         		if (cargapositiva == 1) {
-            			write(file_descriptor, "u", 1);
+            			write(file_descriptor, "UNO", 1);
 				printf("*********************************************************\n");
 				printf("Juego cargado con éxito. Volviendo al menú......\n");
 				printf("*********************************************************\n");
 				continue;
         		}
     		}
+*/
+		switch(opcion)
+		{
+			case 1:
+				printf("¿Desea cargar el UNO?\n");		
+			        printf("1. Sí\n2. No\n");
+				juego = uno();	
+				printf("*********************************************************\n");
+				printf("Juego cargado con éxito. Volviendo al menú......\n");
+				printf("*********************************************************\n");
+				write(file_descriptor, juego, 1);
+				continue;
+			case 2:
+				printf("¿Desea cargar el RUMI?\n1. Sí\n2. No\n");
+				continue;
+			default:
+				continue;
+
+		}		
 		tcdrain(file_descriptor);
 		sleep(1);
 	}
