073da68001c288d59ba2a7bc89e11711e4ee9bb8
Agustin Emanuel Gonzalez <gonzalezagustin27@gmail.com>
2023-11-06 21:28:59 -0300

    nada

--- soft/stepper/stepper.ino
+++ soft/stepper/stepper.ino
@@ -1,22 +1,41 @@
 #include <Stepper.h>
+
+
 uint16_t PasosPorRevolucion = 2048;
-uint8_t motSpeed = 12;
-
-
-Stepper stepper(PasosPorRevolucion,2,3,4,5);
+uint8_t velocidad = 12;
+int juego = 0;
+Stepper stepper(PasosPorRevolucion, 2, 3, 4, 5);
 uint16_t retraso = 1500;
 
 void setup() {
-  // put your setup code here, to run once:
-  stepper.setSpeed(motSpeed);
-  
+  Serial.begin(9600);
+  stepper.setSpeed(velocidad);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  stepper.step(PasosPorRevolucion);
-  delay(retraso);
-  stepper.step(PasosPorRevolucion);
-  delay(retraso);
-  
+  // stepper.step(PasosPorRevolucion);
+  // delay(retraso);
+  // stepper.step(PasosPorRevolucion);
+  // delay(retraso);
+  if (Serial.available() > 0) {
+    juego = Serial.parseInt();  //parseInt lee un entero desde puerto serie
+    juego_seleccionado(juego);
+  }
+}
+
+juego_seleccionado(int juego) {
+  switch(juego){
+    case 1:
+    //llama al a funcion correspondiente al juego 1
+    break;
+    case 2:
+    //llama a la funcion correspondiente al juego 2
+    break;
+    case 3:
+    //llama a la funcion correspondiente al juego 3
+    break;
+    default:
+    break;
+  }
 }
