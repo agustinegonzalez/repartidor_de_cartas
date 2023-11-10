64afbd73295f7d48a45711b6f2744a52ae837bf6
Agustin Emanuel Gonzalez <gonzalezagustin27@gmail.com>
2023-11-09 23:17:24 -0300

    nada

--- soft/stepper/stepper/stepper.ino
+++ soft/stepper/stepper/stepper.ino
@@ -1,9 +1,9 @@
 #include <Stepper.h>
 
 int stepsPerRevolution = 2048;
-uint8_t motSpeed = 12;
+uint8_t motSpeed = 10;
 Stepper myStepper(stepsPerRevolution,2,3,4,5);
-int dt = 1500;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,9 +13,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  myStepper.step(stepsPerRevolution);
-  delay(dt);
-  myStepper.step(-stepsPerRevolution);
-  delay(dt);
-  
+  myStepper.step(stepsPerRevolution);  
 }
