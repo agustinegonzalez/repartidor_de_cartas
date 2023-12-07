8af34f2076476e827051d1fe7d282ea71485ed3d
Agustin Emanuel Gonzalez <gonzalezagustin27@gmail.com>
2023-12-07 20:07:56 -0300

    Uso Serial.readString

--- soft/main_entrega/menu_pc/data_sketch/data_sketch.ino
+++ soft/main_entrega/menu_pc/data_sketch/data_sketch.ino
@@ -5,12 +5,11 @@ void setup() {
 
 void loop() {
   if (Serial.available() > 0) {
-    String cadena = Serial.readStringUntil('\n');
-    cadena.trim(); // Eliminar espacios en blanco, incluido el '\n' al final
+    String cadena = Serial.readString();
+    cadena.trim();
 
     if (cadena == "Cargado") { // Cambié las comillas simples por comillas dobles
-      int numero = 123; // Puedes cambiar este número según tus necesidades
-      Serial.print(numero);
+      Serial.println("Recibido");
     }
   }
 }
